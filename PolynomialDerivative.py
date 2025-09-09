c51e3bb48d8e61bc2291216dd783062317ecb768
Shahaam-Sid <muhammadgc821@gmail.com>
2025-09-09 07:08:12 +0500

    Completed PolynomialDerivative.py

--- PolynomialDerivative.py
+++ PolynomialDerivative.py
@@ -1,6 +1,3 @@
-from http.client import PROCESSING
-
-
 class PolynomialDerivative:
     def __init__(self, degree):
         self.degree = degree
@@ -28,24 +25,33 @@ class PolynomialDerivative:
         
     def equation(self):
         for i in range(self.degree, -1, -1):
-            coeff = int(input(f"x{PolynomialDerivative.int_to_superscript(i)}: "))
+            
+            coeff = int(input("constant: ")) if i == 0 else int(input(f"x{PolynomialDerivative.superscripter(i)}: "))
+            
             if i == 0 or coeff == 0:
                 pass
             else:
                 dydx_coeff = coeff * i
 
-                temp = str(dydx_coeff) + 'x' + str(PolynomialDerivative.int_to_superscript(i - 1))
-                
+                if i == 1:
+                    temp = str(dydx_coeff)
+                elif i == 2:
+                    temp = str(dydx_coeff) + 'x'
+                else:
+                    temp = str(dydx_coeff) + 'x' + str(PolynomialDerivative.superscripter(i - 1))
+                    
                 if temp[0] == '-':
                     self.answer += temp
                 else:
                     self.answer = self.answer + '+' + temp
+                    
+                if self.answer[0] == '+':
+                    self.answer = self.answer[1:]
+                
+        if len(self.answer) == 0:
+            self.answer = "0"
                 
     @staticmethod
-    def int_to_superscript(n):
+    def superscripter(n):
         superscripts = str.maketrans("0123456789-", "⁰¹²³⁴⁵⁶⁷⁸⁹⁻")
         return str(n).translate(superscripts)
-    
-x = PolynomialDerivative(5)
-x.equation()
-print(x.answer)
